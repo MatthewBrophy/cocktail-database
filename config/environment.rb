bed79b958f919f782cdbde02abd86b719dce57bf
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-02-12 14:23:28 -0800

    created database and added some require files to environment.

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+require "sinatra/activerecord"
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/development.sqlite"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
