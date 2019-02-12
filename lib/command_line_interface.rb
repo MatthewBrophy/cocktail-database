81e9e0ff4b7f6a5b979dd5f12fd97add2cb6db7c
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-02-12 14:06:30 -0800

    delete starwars specific references and other unused files. Added relevant files from other projects.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
-def welcome
-  puts "Welcome to Space"
-end
+# def welcome
+#   puts "Welcome to Space"
+# end
 
-def get_character_from_user
-  puts "please enter a character name"
-  gets.strip.downcase
-end
+# def get_character_from_user
+#   puts "please enter a character name"
+#   gets.strip.downcase
+# end
