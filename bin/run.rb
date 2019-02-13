a9404aa2dfe29618ebeef2ca183452ef9fcea5fb
github username <github email address>
2019-02-13 09:52:06 -0800

    working database

--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require_relative "../db/seeds.rb"
 require 'pp'
 
+puts "does run.rb have Drink?"
+#puts Drink
 
 welcome
 
 get_drink_hash_by_id
+
+#get_drink_hash_by_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,14 @@ end
 
 def add_ingredients(drink, drink_hash)
   ingredients = drink_hash.map {|k, v| v if k.start_with?("strIngredient")}
-  ingredients.each do |ingredient_name|
-    ingredient = Ingredient.find_or_create_by_name(ingredient_name)
+  while i < ingredients.length
+    ingredient = Ingriedent.find_or_create_by_name(ingredients[i])
     drink.ingredients << ingredient
+    IngredientCard.create(drink, ingredient)
   end
 end
 
+# ingredients.each do |ingredient_name|
+#   ingredient = Ingredient.find_or_create_by_name(ingredient_name)
+#   drink.ingredients << ingredient
+# end
