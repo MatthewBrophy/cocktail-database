711055b66a77f404151b1b0c4c041464044bf28c
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-02-13 16:11:44 -0800

    built basic initial menu prompt

--- bin/run.rb
+++ bin/run.rb
@@ -1,25 +1,21 @@
-#!/usr/bin/env ruby
-# require_relative "../lib/api_communicator.rb"
-# require_relative "../lib/command_line_interface.rb"
-# require_relative "../db/seeds.rb"
-# require_relative '../app/models/drink'
-# require_relative '../app/models/ingredient_card'
-# require_relative '../app/models/ingredient'
-
 require_relative '../config/environment'
 require 'pry'
 require 'pp'
 require_relative "../app/models/drink.rb"
 
 
-puts Drink
-puts IngredientCard
-puts Ingredient
-
-api_communicator_test
 welcome
+get_user_menu_input
 
-populate_database
+
+
+
+
+
+
+
+
+
 
 # test_hash = {"idDrink"=>"14588",
 #     "strDrink"=>"151 Florida Bushwacker    ",
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,4 +1,55 @@
-def welcome
-  puts "Welcome to Space"
+
+def banner
+puts " _____"
+puts "/.---.\\"
+puts "|`````|"
+puts "\\     /"
+puts " `-.-'           ____"
+puts "   |    /\\     .'   /\\"
+puts " __|__  |K----;    |  |"
+puts "`-----` \\/     '.___\\/"
 end
 
+def welcome
+  puts " "
+  puts " "
+  puts "-------------------------------------------------------"
+  puts "Welcome to the cocktail database.  Let's get you drunk!"
+  puts "-------------------------------------------------------"
+  puts " "
+  puts " "
+  banner
+  puts " "
+  puts " "
+  puts " "
+  puts "---------------------------------------------------------------------------------"
+  puts "Please select from the following options:       (Enter a number between 1 and 4)"
+  puts "---------------------------------------------------------------------------------"
+  puts "1. Learn how to make a cocktail by name"
+  puts "2. Look up a drink list by ingredient"
+  puts "3. Find an ingredient's description"
+  puts "4. Suprise me!"
+  puts "---------------------------------------------------------------------------------"
+end
+
+def get_user_menu_input
+  user_input = gets.strip
+  if user_input == "1"
+    learn_a_cocktail
+  elsif user_input == "2"
+    puts "testy testy 2"
+  elsif user_input == "3"
+    puts "testy testy 3"
+  elsif user_input == "4"
+    puts "testy testy 4"
+  else
+    puts "You may already be too drunk to use this application.  Please make a valid selection"
+    get_user_menu_input
+  end
+end
+
+def learn_a_cocktail
+    puts "Great choice! Please enter the name of the cocktail you would like to learn to make:"
+    user_input = gets.strip.downcase
+    Drink.recipe(user_input)
+end
